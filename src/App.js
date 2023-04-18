795652056d1763c0a9be003267f54778949ba6ac
malucien <124107514+malucien@users.noreply.github.com>
2023-04-17 23:24:40 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import { ChatEngine } from 'react-chat-engine';
-import MessageForm from './components/MessageForm';
 import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
 import './App.css';
 
-// const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+const projectID = "3db55e20-1dc3-4bd1-93e9-c86c2cea1cda";
 
 const App = () => {
   if (!localStorage.getItem('username')) return <LoginForm />;
@@ -13,10 +12,10 @@ const App = () => {
     <ChatEngine
       height="100vh"
       projectID="3db55e20-1dc3-4bd1-93e9-c86c2cea1cda"
-      userName='Lucius'
-      userSecret='0123456789'
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-      // onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
   );
 };
