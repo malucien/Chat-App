de92445c04f02a3523fbbb8914da51f9a7214aa4
malucien <124107514+malucien@users.noreply.github.com>
2023-04-17 21:37:41 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import { ChatEngine } from 'react-chat-engine';
 
-import ChatFeed from './components/ChatFeed';
-import LoginForm from './components/LoginForm';
+// import ChatFeed from './components/ChatFeed';
+// import LoginForm from './components/LoginForm';
 import './App.css';
 
-const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+// const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
 
 const App = () => {
-  if (!localStorage.getItem('username')) return <LoginForm />;
+  // if (!localStorage.getItem('username')) return <LoginForm />;
 
   return (
     <ChatEngine
       height="100vh"
-      projectID={projectID}
-      userName={localStorage.getItem('username')}
-      userSecret={localStorage.getItem('password')}
-      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+      projectID="3db55e20-1dc3-4bd1-93e9-c86c2cea1cda"
+      userName='Lucius'
+      userSecret='0123456789'
+      // renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      // onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
   );
 };
